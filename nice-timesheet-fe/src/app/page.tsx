b01b26dd8dafe54247e3fa9e692c37bb4429c608
Antonio Basile <antoniobasile2@eng.it>
2025-03-23 17:01:07 +0100

    first commit

---
+++ nice-timesheet-fe/src/app/page.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (<>
+          <h1 className="font-bold underline">
+              Hello world!
+          </h1>
+          <Button>
+                Click me!
+          </Button>
+      </>
+  );
+}
