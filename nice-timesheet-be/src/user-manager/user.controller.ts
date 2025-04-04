3063a42f07416d1897d2a1d21ed8a199d1340a7c
christian <christian.luzzetti@gmail.com>
2025-04-04 23:29:09 +0200

    minor: cleanup

--- nice-timesheet-be/src/user-manager/user.controller.ts
+++ nice-timesheet-be/src/user-manager/user.controller.ts
@@ -1,31 +1,40 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { UserService } from './user.service';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {UserService} from './user.service';
 
 @Controller('api/1.0')
 export class UserController {
 
-  constructor(private readonly userService: UserService) {
-  }
+    constructor(private readonly userService: UserService) {
+    }
 
-  @Post('users:createRandom')
-  async createRandomUser() {
-    return this.userService.createRandomUser();
-  }
+    @Post('users:createRandom')
+    async createRandomUser() {
+        return this.userService.createRandomUser();
+    }
 
-  @Get('users/:id')
-  async getUserDetails(@Param('id') id: string) {
-    return this.userService.getUser(id);
-  }
+    @Get('users/:id')
+    async getUserDetails(@Param('id') id: string) {
+        return this.userService.getUser(id);
+    }
 
-  @Post('users')
-  async createUser(@Body() request: UserCreationRequest) {
-    return this.userService.createUser(request);
-  }
+    @Post('users')
+    async createUser(@Body() request: UserCreationRequest) {
+        let createdUser = await this.userService.createUser(request);
+
+        const response: UserCreationResponse = {
+            userId: createdUser.id
+        }
+        return response;
+    }
 
 }
 
 interface UserCreationRequest {
-  firstName: string;
-  lastName: string;
-  email: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+
+interface UserCreationResponse {
+    userId: string;
 }
